5009370ce783b02a6e31d8a1c0aee4ed45808e13
meshach567 <arinzemeshach123@gmail.com>
2025-02-06 16:06:56 +0100

    feat: write the components for App

--- notesapp/src/App.tsx
+++ notesapp/src/App.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="app-container">
+      <form action="" className="note-form">
+        <input type="text" placeholder="Add a note..." required />
+        <textarea name="" id="" placeholder="Content" rows={10} />
+        <button type="submit">Add Note</button>
+      </form>
+      <div className="notes-grid">
+        <div className="note-item">
+          <div className="notes-header">
+            <button type="button">x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note content</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
